f5c6ff71da596a1b63c1ed14594c610e1cf34bba
saran.su <suwannachatkul.s@gmail.com>
2023-05-02 15:59:07 +0900

    Add more param for GET images

--- image_backend/image_api/views.py
+++ image_backend/image_api/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random
 
 from django.shortcuts import render
 from rest_framework.response import Response
@@ -22,13 +23,41 @@ class ImageList(generics.ListAPIView):
 
     def get_queryset(self):
         queryset = ImageInfo.objects.all()
-        tags = self.request.query_params.getlist('tags')
+        # tags filter
+        tags = self.request.query_params.getlist('tags[]') + self.request.query_params.getlist('tags')
         if tags:
             queryset = queryset.filter(tags__name__in=tags)
-        created_date = self.request.query_params.get('created_date')
-        if created_date:
-            date = datetime.strptime(created_date, '%Y-%m-%d')
+
+        # created_date filter
+        created_date_exact = self.request.query_params.get('created_date')
+        if created_date_exact:
+            date = datetime.strptime(created_date_exact, '%Y-%m-%d')
             queryset = queryset.filter(created_at__date=date)
+
+        # create_date range filter
+        created_date_after = self.request.query_params.get('created_date__after')
+        created_date_before = self.request.query_params.get('created_date__before')
+        if created_date_after and created_date_before:
+            date_after = datetime.strptime(created_date_after, '%Y-%m-%d')
+            date_before = datetime.strptime(created_date_before, '%Y-%m-%d')
+            queryset = queryset.filter(created_at__date__range=[date_after, date_before])
+        elif created_date_after:
+            date = datetime.strptime(created_date_after, '%Y-%m-%d')
+            queryset = queryset.filter(created_at__date__gt=date)
+        elif created_date_before:
+            date = datetime.strptime(created_date_before, '%Y-%m-%d')
+            queryset = queryset.filter(created_at__date__lt=date)
+        
+        # random
+        is_random = self.request.query_params.get('random')
+        if is_random == 'true':
+            queryset = queryset.order_by('?')
+
+        # limit
+        limit = self.request.query_params.get('limit')
+        if limit:
+            queryset = queryset[:int(limit)]
+
         return queryset
 
 
