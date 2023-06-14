124ee65ded9990723a134af2152b1c8f5d6bba07
saran.su <suwannachatkul.s@gmail.com>
2023-06-14 16:34:42 +0900

    Refactoring ImageUpload

--- image_backend/image_api/serializers.py
+++ image_backend/image_api/serializers.py
@@ -1,6 +1,5 @@
 import json
 
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.http import QueryDict
 from rest_framework import serializers
 
@@ -11,12 +10,14 @@ from .util.image_util import ImageUtil
 class TagSerializer(serializers.ModelSerializer):
     label = serializers.CharField(source='name')
     value = serializers.CharField(source='name_slug')
+
     class Meta:
         model = Tag
         fields = ('label', 'value')
 
 
 class TagListingField(serializers.RelatedField):
+
     def to_representation(self, value):
         return value.name
 
@@ -30,10 +31,7 @@ class ImageSerializer(serializers.ModelSerializer):
 
 
 class ImageUploadSerializer(serializers.ModelSerializer):
-    MAX_SIZE = 5 * 1024 * 1024  # 1MB
-
-    tags = serializers.ListField(
-        child=serializers.CharField(max_length=50), write_only=True, required=False)
+    tags = serializers.ListField(child=serializers.CharField(max_length=50), write_only=True, required=False)
     tags_info = serializers.SerializerMethodField()
 
     class Meta:
@@ -42,45 +40,7 @@ class ImageUploadSerializer(serializers.ModelSerializer):
 
     def get_tags_info(self, obj):
         tags_data = dict(self.initial_data).get('tags', [])
-        tags_data_alternate = dict(self.initial_data).get('tags[]', [])
-        tags_data.extend(tags_data_alternate)
         return json.dumps(tags_data).replace('\"', '')
-
-    def validate_image(self, image):
-        if image.size > self.MAX_SIZE:
-            resized_image = ImageUtil.optimize_imgsize(image)
-            if not image.name.endswith(".jpg"):
-                image.name = image.name.replace(
-                    image.name.split(".")[-1], "jpg", 1
-                )
-            new_image = InMemoryUploadedFile(
-                resized_image,
-                None,
-                image.name,
-                'image/jpeg',
-                resized_image.__sizeof__,
-                None
-            )
-            if not new_image:
-                raise ValidationError("Could not resize the image.")
-            return new_image
-        return image
-
-    def to_internal_value(self, data):
-        # handle alternate data key for tags "tags[]"
-        if type(data) == QueryDict:
-            tags_data = data.getlist('tags', [])
-            tags_data_alternate = data.getlist('tags[]', [])
-        else:
-            tags_data = data.get('tags', [])
-            tags_data_alternate = data.get('tags[]', [])
-
-        if tags_data_alternate:
-            tags_data.extend(tags_data_alternate)
-
-        validated_data = super().to_internal_value(data)
-        validated_data['tags'] = tags_data
-        return validated_data
 
     def create(self, validated_data):
         tags_data = validated_data.pop('tags')
--- image_backend/image_api/views.py
+++ image_backend/image_api/views.py
@@ -1,8 +1,11 @@
+import json
 import random
 from datetime import datetime
 
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.shortcuts import render
 from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -10,6 +13,7 @@ from rest_framework.views import APIView
 
 from .models import ImageInfo, Tag
 from .serializers import ImageSerializer, ImageUploadSerializer, TagSerializer
+from .util.image_util import ImageUtil
 
 
 class TagList(generics.ListAPIView):
@@ -58,7 +62,7 @@ class ImageList(generics.ListAPIView):
         limit = self.request.query_params.get('limit', None)
         offset = self.request.query_params.get('offset', 0)
         if limit:
-            queryset = queryset[int(offset):int(offset)+int(limit)]
+            queryset = queryset[int(offset):int(offset) + int(limit)]
         else:
             queryset = queryset[int(offset):]
 
@@ -75,10 +79,76 @@ class ImageUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
     permission_classes = [IsAuthenticated]
 
+    SUPPORT_FILE_EXT = ["jpg", "png", "webp"]
+    MAX_IMG_SIZE = 2 * 1024 * 1024  # 2MB
+
     def post(self, request, *args, **kwargs):
-        serializer = ImageUploadSerializer(data=request.data)
+        # data
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        tags = request.POST.getlist('tags[]')
+        image = request.FILES.get('image')
+
+        # param
+        file_ext = self.request.query_params.get('file_ext')
+
+        try:
+            if file_ext:
+                self.__validate_file_ext(file_ext)
+            image_valid = self.__validate_image(image, transform_ext=file_ext)
+
+        except ValidationError as error:
+            return Response({'error': error.detail}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error': 'Image pre-process validation error'}, status=status.HTTP_400_BAD_REQUEST)
+
+        data = {
+            'title': title,
+            'description': description,
+            'tags': tags,
+            'image': image_valid,
+        }
+        serializer = ImageUploadSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def __validate_file_ext(self, file_ext):
+        if file_ext not in self.SUPPORT_FILE_EXT:
+            raise ValidationError(f'Not Support file_ext: {file_ext}')
+        return True
+
+    def __validate_image(self, image, transform_ext):
+        # size exceeded do resize
+        if image.size > self.MAX_IMG_SIZE:
+            if not transform_ext:
+                return self.__resize_image(image, 'jpeg', self.MAX_IMG_SIZE)
+            else:
+                return self.__resize_image(image, transform_ext, self.MAX_IMG_SIZE)
+
+        # size not exceeded but image ext needed to covert
+        if transform_ext and not image.name.endswith("." + transform_ext):
+            image = self.__convert_image(image, transform_ext)
+
+        return image
+
+    def __resize_image(self, image, transform_ext, target_size):
+        resized_image = ImageUtil.optimize_image_bytes_size(image.read(), transform_ext, target_size)
+        return self.__create_memory_upload_file(resized_image, image.name, transform_ext)
+
+    def __convert_image(self, image, transform_ext):
+        converted_image = ImageUtil.convert_image_type(image.read(), transform_ext)
+        converted_image = ImageUtil.PIL_to_bytes(converted_image, transform_ext)
+        return self.__create_memory_upload_file(converted_image, image.name, transform_ext)
+
+    def __create_memory_upload_file(self, image, image_name, ext):
+        if not image_name.endswith("." + ext):
+            image_name = image_name.replace(image_name.split(".")[-1], ext, 1)
+
+        new_image = InMemoryUploadedFile(image, None, image_name, 'image/' + ext, image.__sizeof__, None)
+        if not new_image:
+            raise ValidationError("Could not resize the image.")
+
+        return new_image
